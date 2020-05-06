7cfe13adf69c7432d0db517e6539e6304b49308a
DiFine <biebel2003@gmail.com>
2020-05-06 20:15:52 +0400

    feat: add gulp-sass in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,38 @@
-const gulp = require('gulp');
+const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
-const cssFiles = ('./css/*.css')
+const cssFiles = ('./css/*.css');
 const cleanCSS = require('gulp-clean-css');
-const rename = require('gulp-rename')
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
 
 
-gulp.task('browser-sync', function() {
-  browserSync.init({
-      server: {
-          baseDir: "./"
-      }
-  });
-  gulp.watch("./*.html").on('change', browserSync.reload);
-});
+  function bs(){
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    watch("./*.html").on('change', browserSync.reload);
+    watch("./sass/**/*.sass", serveSass);
+    watch("./js/*.js").on('change', browserSync.reload);
+    watch("./css/*.css", styles);
+  };
 
-gulp.task('styles', function () {
+  function serveSass() {
+    return src("./sass/*.sass")
+        .pipe(sass())
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
+  };
 
-  return gulp.src(cssFiles)
-  .pipe(cleanCSS({level: 2}))
-  .pipe(rename({
-    suffix: '.min'
-  }))
-  .pipe(gulp.dest('./style'))
+  function styles() {
 
-});
+    return src(cssFiles)
+    .pipe(cleanCSS({level: 2}))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(dest('./style'))
+  
+  };
+exports.serve = bs;
