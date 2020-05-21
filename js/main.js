20c17f319f86c578e3fe7a583cc1a754700afa2d
DiFine <biebel2003@gmail.com>
2020-05-21 10:06:06 +0400

    fix: fix bugs

--- js/main.js
+++ js/main.js
@@ -233,7 +233,7 @@ $(document).ready(function () {
   ymaps.ready(function () {
     var myMap = new ymaps.Map('map', {
             center: [55.751574, 37.573856],
-            zoom: 9
+            zoom: 12
         }, {
             searchControlProvider: 'yandex#search'
         }),
